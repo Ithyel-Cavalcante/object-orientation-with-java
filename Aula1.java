0202bf320a3e8ae4f4178997f083de111ba78222
Ithyel Vasconcelos <ithyelvasconcelos418@gmail.com>
2024-02-14 14:58:58 -0300

    commmit att

---
+++ Aula1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Aula1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int soma, a, b;
+
+        System.out.println("Digite um número: \n");
+       a = input.nextInt();
+
+        System.out.println("Digite um segundo número: ");
+       b = input.nextInt();
+
+       soma = a + b;
+
+        System.out.println("A soma dos dois numeros é: "+soma);
+
+    }
+    }
